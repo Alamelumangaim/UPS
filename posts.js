fe900b52d4ebb683f2e92786652bb1b1c05a1f10
Alamelu Mangai <alamelusowmi5624@gmail.com>
2023-10-06 16:48:09 +0530

    changes

---
+++ posts.js
@@ -0,0 +1,33 @@
+module.exports=[
+    {
+      "id": 1,
+      "name": "John Doe",
+      "age": 30,
+      "city": "New York"
+    },
+    {
+      "id": 2,
+      "name": "Alice Johnson",
+      "age": 25,
+      "city": "Los Angeles"
+    },
+    {
+      "id": 3,
+      "name": "Bob Smith",
+      "age": 35,
+      "city": "Chicago"
+    },
+    {
+      "id": 4,
+      "name": "Eve Brown",
+      "age": 28,
+      "city": "San Francisco"
+    },
+    {
+      "id": 5,
+      "name": "Charlie Wilson",
+      "age": 32,
+      "city": "Houston"
+    }
+  ]
+  
