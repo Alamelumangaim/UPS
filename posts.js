3ca9a000ff986fc358e0dd77eee3de167af9c2a9
Alamelu Mangai <alamelusowmi5624@gmail.com>
2023-10-06 17:13:14 +0530

    json changes

--- posts.js
+++ posts.js
@@ -1,33 +1,17 @@
 module.exports=[
-    {
-      "id": 1,
-      "name": "John Doe",
-      "age": 30,
-      "city": "New York"
-    },
-    {
-      "id": 2,
-      "name": "Alice Johnson",
-      "age": 25,
-      "city": "Los Angeles"
-    },
-    {
-      "id": 3,
-      "name": "Bob Smith",
-      "age": 35,
-      "city": "Chicago"
-    },
-    {
-      "id": 4,
-      "name": "Eve Brown",
-      "age": 28,
-      "city": "San Francisco"
-    },
-    {
-      "id": 5,
-      "name": "Charlie Wilson",
-      "age": 32,
-      "city": "Houston"
-    }
-  ]
-  
+  {
+    "id": 1,
+    "title": "Post 1",
+    "content": "This is the content of post 1."
+  },
+  {
+    "id": 2,
+    "title": "Post 2",
+    "content": "This is the content of post 2."
+  },
+  {
+    "id": 3,
+    "title": "Post 3",
+    "content": "This is the content of post 3."
+  }
+]
